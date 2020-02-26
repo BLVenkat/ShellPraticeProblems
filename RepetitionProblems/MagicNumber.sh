b83c3ee2b3a808039748b791b1abecad5e7cb9ea
nikhiljainlive <nikhiljain5195@gmail.com>
2020-02-27 00:42:11 +0530

    [Nikhil]Refactor. updated MagicNumber.sh file with logic for finding magic number

--- RepetitionProblems/MagicNumber.sh
+++ RepetitionProblems/MagicNumber.sh
@@ -1,1 +1,31 @@
 #!/bin/bash -x
+
+# constants
+YES=Y
+NO=N
+
+# variables
+lowerLimit=0
+upperLimit=100
+magicNum=50
+
+read -p "Think of a magic number in a range of 0-100 and press any key!"
+
+while [[ $lowerLimit -lt $upperLimit ]]
+do
+	read -p "Is the number greater than $magicNum? (Y or N) : " isGreaterThanMagicNum
+
+	if [[ $isGreaterThanMagicNum == $YES ]]
+	then
+		lowerLimit=$(( $magicNum + 1 ))
+	elif [[ $isGreaterThanMagicNum == $NO ]]
+	then
+		upperLimit=$magicNum
+	else
+		echo "Invalid Input! Please try again..."
+	fi
+
+	magicNum=$(( $(( $lowerLimit + $upperLimit )) / 2 ))
+done
+
+echo "The Magic Number is : $magicNum"
