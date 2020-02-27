95674e223fd21a9d4eb7f8978a66e5a7875cf2f7
Hayaati <hayaatigowda159@gmail.com>
2020-02-27 09:06:53 +0530

    [vidya]Add. solved springseason problem

---
+++ springSeason.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+day=$1
+month=$2
+month=${month,,}
+result=false
+
+if [[ ( "$month" = "march" && $day -ge 20 && $day -le 31 ) || 
+      ( "$month" = "april" && $day -ge 1 && $day -le 30 ) || 
+      ( "$month" = "may" && $day -ge 1 && $day -le 31 ) || 
+      ( "$month" = "june" && $day -ge 1 && $day -le 20 ) ]]
+then
+       result=true
+fi
+
+echo "Date: $day-$month"
+echo "Reslut is, $result"/home/bridgelabz/Desktop/springSeason.sh
