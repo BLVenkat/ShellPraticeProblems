e36d1a5fb925014449bb93a40f35b36b9e829967
varahs <varshanataraj2012@gmail.com>
2020-02-27 10:41:16 +0530

    [varsha]add. problem statement included

--- gambler.sh
+++ gambler.sh
@@ -1,4 +1,8 @@
-#/bin/bash -x
+#!/bin/bash -x
+
+#Write a Program where a gambler starts with Rs 100 and places Re 1 bet
+#until he/she goes broke i.e. no more money to gamble or reaches the
+#goal of Rs 200. Keeps track of number of times won and number of bets made.(Use While loop)
 
 STAKE=100;
 BET=1;
@@ -25,3 +29,4 @@ done
 echo "win = $win"
 echo "loss = $loss"
 echo "Bets = $bets"
+
