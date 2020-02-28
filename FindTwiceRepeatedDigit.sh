4613f644d31d8fa84603f1d2148eb25e815162d3
manish7111 <mmkr7111@gmail.com>
2020-02-28 17:58:28 +0530

    [Manish Reddy]Refactor. Made changes in the indentation of the program

--- FindTwiceRepeatedDigit.sh
+++ FindTwiceRepeatedDigit.sh
@@ -1,6 +1,5 @@
 # --------------------------------------------------------------------------------------------------------------------
 # <copyright file="FindTwiceRepeatedDigit.sh" name="Manish Reddy" company="Bridgelabz"></copyright>
-
 # Problem Statement :-
 #	 Take a range from 0 –100, find the digits that are repeated twice like 33, 77, etc and store them in an array
 # --------------------------------------------------------------------------------------------------------------------
