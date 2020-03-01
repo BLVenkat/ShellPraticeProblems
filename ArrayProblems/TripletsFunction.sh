82c2551ad537fe6c7cef9dc0649f0ae1725ea141
nikhiljainlive <nikhiljain5195@gmail.com>
2020-03-01 23:41:07 +0530

    [Nikhil]Refactor. refactored method names and commenting in TripletsFunction.sh

--- ArrayProblems/TripletsFunction.sh
+++ ArrayProblems/TripletsFunction.sh
@@ -1,6 +1,6 @@
 #Problem statement              :- Write a program to show sum of three integer adds to zero.
 #Output                         :- Prints the Three Numbers of array whose sum is 0.
-#file                           :- Triplets.sh
+#file                           :- TripletsFunction.sh
 
 #!/bin/bash -x
 
@@ -8,16 +8,16 @@ echo "Enter the elements in array separated by white-space : "
 read -a ARRAY
 ARRAY_LENGTH=${#ARRAY[@]}
 
-# calculate triplets 
+# function to loop through array for third index
 # params - firstIndex, secondIndex
-function calculate_triplets() {
+function calculate_for_third_index() {
 	local firstIndex=$1
 	local secondIndex=$2
 	for (( thirdIndex = (( secondIndex + 1 )); thirdIndex < ARRAY_LENGTH; thirdIndex++ ))
 	do
-		firstElement=${ARRAY[$firstIndex]}
-		secondElement=${ARRAY[$secondIndex]}
-		thirdElement=${ARRAY[$thirdIndex]}
+		local firstElement=${ARRAY[$firstIndex]}
+		local secondElement=${ARRAY[$secondIndex]}
+		local thirdElement=${ARRAY[$thirdIndex]}
 		if [ $(( $firstElement + $secondElement + $thirdElement )) -eq 0 ]
 		then
 			echo $firstElement $secondElement $thirdElement
@@ -27,16 +27,21 @@ function calculate_triplets() {
 
 # function to loop through array for second index
 # params - firstIndex 
-function repeat_for_second_index() {
+function calculate_for_second_index() {
 	local firstIndex=$1
 	for (( secondIndex = (( firstIndex + 1 )); secondIndex < (( ARRAY_LENGTH - 1 )); secondIndex++ ))
 	do
-		calculate_triplets $firstIndex $secondIndex
+		calculate_for_third_index $firstIndex $secondIndex
+	done
+}
+
+# function to find triplets
+function find_triplets() {
+	for (( firstIndex = 0; firstIndex < ARRAY_LENGTH - 2; firstIndex++ ))
+	do
+		calculate_for_second_index $firstIndex
 	done
 }
 
 #main
-for (( firstIndex = 0; firstIndex < ARRAY_LENGTH - 2; firstIndex++ ))
-do
-	repeat_for_second_index $firstIndex
-done
+find_triplets
